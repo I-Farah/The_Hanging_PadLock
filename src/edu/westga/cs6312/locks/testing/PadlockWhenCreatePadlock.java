fdbbb7445ad802b4ebc1db786c3f3ff9859dccb0
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-01-17 07:02:00 -0500

    created a second test to test the combination 4, 5, 6 in the padlock testing class

--- src/edu/westga/cs6312/locks/testing/PadlockWhenCreatePadlock.java
+++ src/edu/westga/cs6312/locks/testing/PadlockWhenCreatePadlock.java
@@ -25,4 +25,15 @@ public class PadlockWhenCreatePadlock {
 		String result = plainLock.toString();
 		assertEquals("This Padlock has the combination (1, 2, 3), is currently at 0, and cannot be opened", result);
 	}
+	
+	/**
+	 * Test to see if the 3-parameter correctly creates a Padlock with combination
+	 * (4, 5, 6)
+	 */
+	@Test
+	public void testPadLockWithCombination456() {
+		Padlock plainLock = new Padlock(4, 5, 6);
+		String result = plainLock.toString();
+		assertEquals("This Padlock has the combination (4, 5, 6), is currently at 0, and cannot be opened", result);
+	}
 }
