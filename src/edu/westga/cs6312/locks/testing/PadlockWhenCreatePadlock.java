3ed162e2c0778b4e1fe50c406ce1e0b6c4ba588c
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-01-17 06:28:15 -0500

    tested the Padlock class using the PadlockWhenCreatePadlock class with passing results and appropriate comments

--- src/edu/westga/cs6312/locks/testing/PadlockWhenCreatePadlock.java
+++ src/edu/westga/cs6312/locks/testing/PadlockWhenCreatePadlock.java
@@ -1,14 +1,28 @@
 package edu.westga.cs6312.locks.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class PadlockWhenCreatePadlock {
+import edu.westga.cs6312.locks.model.Padlock;
 
+/**
+ * This is a class to test the Padlock class
+ * 
+ * @author Ibrahim Tonifarah
+ * @version 1/17/2019
+ * 
+ */
+public class PadlockWhenCreatePadlock {
+
+	/**
+	 * Test to see if the 3-parameter correctly creates a Padlock with combination
+	 * (1, 2, 3)
+	 */
 	@Test
-	void testToString() {
-		fail("Not yet implemented");
+	public void testPadLockWithCombination123() {
+		Padlock plainLock = new Padlock(1, 2, 3);
+		String result = plainLock.toString();
+		assertEquals("This Padlock has the combination (1, 2, 3), is currently at 0, and cannot be opened", result);
 	}
-
 }
