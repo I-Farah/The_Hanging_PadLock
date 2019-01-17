07c1ccfbc8c586cd100f3f667889288f5308c4ed
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-01-17 04:57:39 -0500

    initial repository where a package was created with a Padlock class
    That contained instance variables

---
+++ src/edu/westga/cs6312/locks/model/Padlock.java
@@ -0,0 +1,19 @@
+
+package edu.westga.cs6312.locks.model;
+
+/**
+ * This class represents a model free hanging padlock with three combinations
+ * and its functionality
+ * 
+ * @author Ibrahim Tonifarah
+ * @version 1/17/2019
+ *
+ */
+public class Padlock {
+	private int lockCombinationNumber1;
+	private int lockCombinationNumber2;
+	private int lockCombinationNumber3;
+	private int currentLockNumber;
+	private boolean canLockOpen;
+	
+}
